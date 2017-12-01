0d97b9349a3da737e6691255d610831a9b3f41e3
JFletcher1 <joe-fletcher@live.co.uk>
2017-12-01 15:20:29 +0000

    Finished search Add all fields

---
+++ src/test/java/AutoTraderTest.java
@@ -0,0 +1,125 @@
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader;
+import org.junit.*;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class AutoTraderTest {
+    private WebDriver webDriver;
+    private static ExtentReports report;
+    ScreenShot screenShot = new ScreenShot();
+    private ExtentTest test;
+    private SpreadSheetReader spreadSheetReader;
+    private String url;
+
+    @BeforeClass
+    public static void init() {
+        report = new ExtentReports();
+        String fileName = "MyReport" + ".html";
+        String filePath = System.getProperty("user.dir")
+                + File.separatorChar + fileName;
+        report.attachReporter(new ExtentHtmlReporter(filePath));
+    }
+    @AfterClass
+    public static void cleanUp() {
+        report.flush();
+    }
+
+    @Before
+    public void setUp() {
+        spreadSheetReader = new SpreadSheetReader("testData.xlsx");
+        List<String> inputList;
+        inputList = spreadSheetReader.readRow(1, "Sheet1");
+        url = inputList.get(0).toString();
+        if (inputList.get(1).equals("Chrome"))
+        {
+            webDriver = new ChromeDriver();
+        } else if (inputList.get(1).equals("Firefox")) {
+            System.setProperty("webdriver.gecko.driver","geckodriver.exe");
+            webDriver = new FirefoxDriver();
+        } else {
+            test.log(Status.FATAL,"No Supported Browser Selected");
+        }
+    }
+    @After
+    public void tearDown() {
+        webDriver.quit();
+    }
+
+    @Test
+    public void searchForCars()
+    {
+        spreadSheetReader = new SpreadSheetReader("testData.xlsx");
+        List<String> inputList;
+        inputList = spreadSheetReader.readRow(1, "Sheet1");
+        ExtentTest test = report.createTest("searchForCars");
+        HomePage homepage = PageFactory.initElements(webDriver, HomePage.class);
+        test.log(Status.INFO, "Going to url - " + url);
+        webDriver.navigate().to(url);
+        test.log(Status.INFO, "Entering the postcode: " + inputList.get(2));
+        homepage.enterPostcode(inputList.get(2));
+        test.log(Status.INFO, "Entering the radius: " + floatToString(inputList.get(3)));
+        homepage.enterRadius(floatToString(inputList.get(3)));
+        test.log(Status.INFO, "Setting used checked status: " + inputList.get(4));
+        if (inputList.get(4).equals("unchecked")) homepage.clickUsedChecked();
+        test.log(Status.INFO, "Setting nearly used status: " + inputList.get(5));
+        if (inputList.get(5).equals("unchecked")) homepage.clickNearlyUsedChecked();
+        test.log(Status.INFO, "Setting new status: " + inputList.get(6));
+        if (inputList.get(6).equals("unchecked")) homepage.clickNewChecked();
+        mySleep();
+        test.log(Status.INFO, "Entering the make: " + inputList.get(7));
+        homepage.enterMake(inputList.get(7));
+        mySleep();
+        test.log(Status.INFO, "Entering the model: " + inputList.get(8));
+        homepage.enterModel(inputList.get(8));
+        mySleep();
+        test.log(Status.INFO, "Entering min price: " + floatToString(inputList.get(9)));
+        homepage.enterMinPrice(floatToString(inputList.get(9)));
+        mySleep();
+        test.log(Status.INFO, "Entering max price: " + floatToString(inputList.get(10)));
+        homepage.enterMaxPrice(floatToString(inputList.get(10)));
+        test.log(Status.INFO, "Taking screenshot");
+
+        try {
+            String path = screenShot.take(webDriver, "image");
+            test.addScreenCaptureFromPath(path);
+        } catch (IOException e) {
+            test.log(Status.FAIL,"Failed taking or adding the screenshot");
+        }
+
+        test.log(Status.INFO,"submitting search query; Expected URL:"+inputList.get(11));
+        homepage.clickSearch();
+        try {
+            Assert.assertEquals(inputList.get(11), webDriver.getCurrentUrl());
+            test.log(Status.PASS,"Passed url comparison assertion");
+        } catch (AssertionError  e) {
+            test.log(Status.FAIL,"Failed url comparison assertion");
+        }
+    }
+    @Test
+    public void createAdd()
+    {
+
+    }
+    private void mySleep()
+    {
+        try {
+            Thread.sleep(200);
+        } catch (Exception e) {}
+    }
+    private String floatToString(String input)
+    {
+        return String.valueOf((int) Float.parseFloat(input));
+    }
+}
---
+++ src/test/java/HomePage.java
@@ -0,0 +1,96 @@
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+
+public class HomePage {
+    @FindBy(css = "#js-channel-nav > ul.other-vehicles__list > li:nth-child(1) > a")
+    private WebElement carsTab;
+    @FindBy(css = "#js-channel-nav > ul.other-vehicles__list > li:nth-child(2) > a")
+    private WebElement prestigeCarsTab;
+    @FindBy(css = "#js-channel-nav > ul.other-vehicles__list > li:nth-child(3) > a")
+    private WebElement vansTab;
+    @FindBy(id = "postcode")
+    private WebElement postcode;
+    @FindBy(id = "radius")
+    private WebElement radius;
+    @FindBy(css = "#searchVehicles > fieldset > fieldset > label:nth-child(2)")
+    private WebElement usedChecked;
+    @FindBy(css = "#searchVehicles > fieldset > fieldset > label:nth-child(3)")
+    private WebElement nearlyNewChecked;
+    @FindBy(css = "#searchVehicles > fieldset > fieldset > label:nth-child(4)")
+    private WebElement newChecked;
+    @FindBy(id = "searchVehiclesMake")
+    private WebElement make;
+    @FindBy(id = "searchVehiclesModel")
+    private WebElement model;
+    @FindBy(id = "searchVehiclesPriceFrom")
+    private WebElement minPrice;
+    @FindBy(id = "searchVehiclesPriceTo")
+    private WebElement maxPrice;
+    @FindBy(id = "js-search-button")
+    private WebElement search;
+    @FindBy(css = "#js-sell-module > form > input:nth-child(1)")
+    private WebElement regNo;
+    @FindBy(css = "#js-sell-module > form > input.c-form__input.no-spinner")
+    private WebElement miles;
+
+
+    public void enterPostcode(String postcode)
+    {
+        this.postcode.sendKeys(postcode);
+    }
+    public void enterRadius(String radius)
+    {
+        Select select = new Select(this.radius);
+        select.selectByValue(radius);
+    }
+    public void clickUsedChecked()
+    {
+        usedChecked.click();
+    }
+    public void clickNearlyUsedChecked()
+    {
+        nearlyNewChecked.click();
+    }
+    public void clickNewChecked()
+    {
+        newChecked.click();
+    }
+    public void enterMake(String make)
+    {
+        Select select = new Select(this.make);
+        select.selectByValue(make);
+    }
+    public void enterModel(String model)
+    {
+        Select select = new Select(this.model);
+        select.selectByValue(model);
+    }
+    public void enterMinPrice(String minPrice)
+    {
+        Select select = new Select(this.minPrice);
+        select.selectByValue(minPrice);
+    }
+    public void enterMaxPrice(String maxPrice)
+    {
+        Select select = new Select(this.maxPrice);
+        select.selectByValue(maxPrice);
+    }
+    public void clickSearch()
+    {
+        search.click();
+    }
+
+    public void gotoCars()
+    {
+        carsTab.click();
+    }
+    public void gotoPrestigeCarsTab()
+    {
+        prestigeCarsTab.click();
+    }
+    public void gotoVansTab()
+    {
+        vansTab.click();
+    }
+}
